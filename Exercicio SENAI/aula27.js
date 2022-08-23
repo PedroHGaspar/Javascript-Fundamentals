042f0881e4e7010d3ebc4056a915545302583b05
Pedro Henrique Gaspar de Figueiredo <ordep12@hotmail.com.br>
2022-08-23 10:44:15 -0300

    Excs 27/29

---
+++ Exercicio SENAI/aula27.js
@@ -0,0 +1,6 @@
+const pi = 3.14;
+var raio = 2.3;
+
+var area = pi * (raio * raio);
+
+console.log(area);
---
+++ Exercicio SENAI/aula29.js
@@ -0,0 +1,8 @@
+var a = 1;
+var b = 2;
+var c = a
+a = b
+b = c
+
+console.log(a);
+console.log(b);
